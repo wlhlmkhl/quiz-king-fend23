fe547124b00639988346193857fc173ba9aa1c2d
wlhlmkhl <wilhelm.kohl123@gmail.com>
2023-12-07 13:34:32 +0100

    day 5 part1

--- question.js
+++ question.js
@@ -10,26 +10,6 @@ const questions = [
 
     },
     {
-        type:"trueOrFalse",
-        question: 'There is better cheese in Sweden than in Norway?',
-        answers: [
-            { text: 'True', correct: true },
-            { text: 'False', correct: false },
-           
-        ]
-
-    },
-    {
-        type:"trueOrFalse",
-        question: 'There is better cheese in Sweden than in Norway?',
-        answers: [
-            { text: 'True', correct: true },
-            { text: 'False', correct: false },
-           
-        ]
-
-    },
-    {
         type: "oneOfFour",
         question: 'What is the capital of France?',
         answers: [
@@ -39,56 +19,7 @@ const questions = [
             { text: 'Madrid', correct: false }
         ]
     },
-    {   
-        type:"oneOfFour",
-        question: 'Which planet is known as the Red Planet?',
-        answers: [
-            { text: 'Earth', correct: false },
-            { text: 'Mars', correct: true },
-            { text: 'Jupiter', correct: false },
-            { text: 'Venus', correct: false }
-        ]
-    },
-    {   
-        type:"oneOfFour",
-        question: 'Which planet is known as the Red Planet?',
-        answers: [
-            { text: 'Earth', correct: false },
-            { text: 'Mars', correct: true },
-            { text: 'Jupiter', correct: false },
-            { text: 'Venus', correct: false }
-        ]
-    },
-    {   
-        type:"multiChoice",
-        question: 'Which two langues does Wille Speak',
-        answers: [
-            { text: 'Finnish', correct: false },
-            { text: 'Swedish', correct: true },
-            { text: 'English', correct: true },
-            { text: 'Turkish', correct: false }
-        ]
-    },
-    {   
-        type:"multiChoice",
-        question: 'Which two langues does Wille Speak',
-        answers: [
-            { text: 'Finnish', correct: false },
-            { text: 'Swedish', correct: true },
-            { text: 'English', correct: true },
-            { text: 'Turkish', correct: false }
-        ]
-    },
-    {   
-        type:"multiChoice",
-        question: 'Which two langues does Wille Speak',
-        answers: [
-            { text: 'Finnish', correct: false },
-            { text: 'Swedish', correct: true },
-            { text: 'English', correct: true },
-            { text: 'Turkish', correct: false }
-        ]
-    },
+
     {   
         type:"multiChoice",
         question: 'Which two langues does Wille Speak',
@@ -99,5 +30,6 @@ const questions = [
             { text: 'Turkish', correct: false }
         ]
     }
+  
     // Add more questions as needed
 ];
--- script.js
+++ script.js
@@ -9,37 +9,64 @@ let title = document.querySelector("#title");
 // global variables and functions
 let currentIndex = 0;
 let rightAnswers = 0;
+let counter = 0;
 
 function updateTitle(){
     title.textContent= `Question ${currentIndex + 1} of ${questions.length}`
 };
-function processAnswer(elem){
-    //get siblings and selected
-    let siblings = Array.from(elem.target.parentNode.children).filter(function (sibling) {
-        return sibling !== elem.target;});
+
+function processAnswer1(elem, counter){
     let selected = elem.target;
     //give class
-        if(selected.value === "true"){
-            selected.classList.add("correct");
-            rightAnswers++;
-        }
-        else{
-            selected.classList.add("incorrect");
-        }
-    siblings.forEach((sibling)=>{
-        sibling.disabled = true;
-    })
+    if(selected.value === "true"){
+        selected.classList.add("correct");
+        rightAnswers++;
+    }
+    else{
+        selected.classList.add("incorrect");
+    };
+    //disable sibling
+    let siblings = Array.from(elem.target.parentNode.children).filter(function (sibling) {
+        return sibling !== elem.target;});
+        siblings.forEach((sibling)=>{
+            sibling.disabled = true;
+        });
+       
     
 };
+function processAnswer2(elem){
+    let siblings = Array.from(elem.target.parentNode.children);
+    let selected = elem.target;
+    const maxSelections = 2;
+    if (counter < maxSelections) {
+
+        if (selected.value === "true") {
+            selected.classList.add("correct");
+        } else {
+            selected.classList.add("incorrect");
+        };
+        counter++;
+
+        // Disable the selected button after processing the answer
+        selected.disabled = true;
+    }
+    else if(counter===maxSelections){
+        console.log("blipp");
+    };
+    
+
+       
+};
+
 
 //Start Game!
 startBtn.addEventListener("click", startGame);
 function startGame(){
+    currentIndex = 0;
+    updateTitle();
     startBtn.classList.add("hide");
     nextBtn.classList.remove("hide");
     questionText.classList.remove("hide");
-    updateTitle();
-    currentIndex = 0;
     answerContainer.innerHTML ="";
     showQuestion(currentIndex);
 };
@@ -53,6 +80,7 @@ function showQuestion(index){
         btn.classList.add("btn");
         btn.innerHTML = answer.text;
         btn.value = answer.correct;
+        btn.dataset.type = questions[index].type;
         answerContainer.appendChild(btn);
         nextBtn.classList.add("hide");
 
@@ -69,13 +97,19 @@ function nextQuestion(){
 };
 //Capture the right answer
 answerContainer.addEventListener("click", (elem)=>{
-//1. Disable alla sibilings
-processAnswer(elem);
-//2. Ta bort classen selected från alla siblings
-if(questions.length === currentIndex + 1){
-    startBtn.textContent = 'Restart';
-    startBtn.classList.remove("hide");
-}
-else
+if(elem.target!==answerContainer){
+
+    if(elem.target.dataset.type ==="multiChoice"){
+        processAnswer2(elem);
+    }
+    else{
+        processAnswer1(elem);
+    }
+    if(questions.length === currentIndex + 1){
+        startBtn.textContent = 'Restart';
+        startBtn.classList.remove("hide");
+    }
+    else
     nextBtn.classList.remove("hide");
+};
 });
